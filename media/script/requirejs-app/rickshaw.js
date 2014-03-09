dd285f1eb6f3c871c6edf24cbf5868935d36d3e5
B. van Berkum <dev@dotmpe.com>
2014-03-10 00:25:48 +0100

    Don't understand it. Returning to original seed.

--- media/script/requirejs-app/rickshaw.js
+++
@@ -1,5 +0,0 @@
-define(["rickshaw", 'd3'], function(Rickshaw) {
-  console.log(['rickshaw should be loaded', Rickshaw]);
-  return Rickshaw;
-});
-
---
+++ media/script/toolkit.js
@@ -0,0 +1,7 @@
+define('toolkit', ['jquery', 'underscore'], function(jQuery, _) {
+  console.log('jQ should be loaded');
+  console.log(jQuery);
+  console.log('_ should be loaded');
+  console.log(_);
+})
+
